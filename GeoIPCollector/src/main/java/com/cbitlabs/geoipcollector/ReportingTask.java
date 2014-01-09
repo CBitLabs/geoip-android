64524956b7548c9e1fb7523b78e94ec432f07e5e
Joshua Blum <jblum@bitsighttech.com>
2014-01-09 11:29:47 -0500

    naming refactor

---
+++ GeoIPCollector/src/main/java/com/cbitlabs/geoipcollector/ReportingTask.java
@@ -0,0 +1,48 @@
+package com.cbitlabs.geoipcollector;
+
+/**
+ * Created by stuart on 11/25/13.
+ */
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+public class ReportingTask extends AsyncTask {
+
+    private Context context;
+
+    public ReportingTask(Context context) {
+        Log.d(Util.TAG, "DataTask Created");
+
+        this.context = context;
+    }
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        Log.d(Util.TAG, "DataTask.doInBackground()");
+
+        this.postReport();
+        return null;
+    }
+
+    public void postReport() {
+        JsonObject json = new JsonObject();
+        json.addProperty("foo", "bar");
+
+        Ion.with(context, Util.getReportServerUrl())
+                .setJsonObjectBody(json)
+                .asJsonObject()
+                .setCallback(new FutureCallback<JsonObject>() {
+                    @Override
+                    public void onCompleted(Exception e, JsonObject result) {
+                        Log.i(Util.TAG, ("Res: %s, err: %s").format(result.toString(), e.toString()));
+                    }
+                });
+    }
+}
+
