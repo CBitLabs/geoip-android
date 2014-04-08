c9443b0a299a6b8067719e694df91b55d10821ef
meredrica <stuff@meredrica.org>
2014-04-09 01:14:16 +0200

    encapsulates scanning into a fragment

--- GeoIP/src/main/java/com/cbitlabs/geoip/MainActivity.java
+++ GeoIP/src/main/java/com/cbitlabs/geoip/MainActivity.java
@@ -1,275 +1,60 @@
 package com.cbitlabs.geoip;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.koushikdutta.async.future.Future;
-import com.koushikdutta.async.future.FutureCallback;
-import com.koushikdutta.ion.Ion;
-import com.koushikdutta.ion.builder.Builders;
+import com.cbitlabs.geoip.fragments.ScanFragment;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements ActionBar.TabListener {
 
-	private ScanAdapter scanAdaptor = null;
-	private Timer autoUpdate;
-	private final int TWENTY_SECONDS = 20 * 1000;
-	private Menu menu;
-
-	// This "Future" tracks loading operations.
-	// A Future is an object that manages the state of an operation
-	// in progress that will have a "Future" result.
-	// You can attach callbacks (setCallback) for when the result is ready,
-	// or cancel() it if you no longer need the result.
-	Future<JsonObject> loading;
+	private static final String CURRENT_TAB = "com.cbitlabs.geoip.MainActivity.CURRENT_TAB";
+	private Tab networks;
+	private Tab history;
+	private int currentTab;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		getActionBar().setDisplayShowHomeEnabled(false);
-
-		// start reporting geoIP in the background.
+		ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		networks = actionBar.newTab().setText(R.string.available_networks);
+		history = actionBar.newTab().setText(R.string.history);
+		history.setTabListener(this);
+		networks.setTabListener(this);
+		actionBar.addTab(networks);
+		actionBar.addTab(history);
+		currentTab = savedInstanceState == null ? 0 : savedInstanceState.getInt(CURRENT_TAB, 0);
+		actionBar.setSelectedNavigationItem(currentTab);
+		actionBar.setDisplayShowHomeEnabled(false);
 		startService(new Intent(getApplicationContext(), ReportIntentService.class));
-
-		setScanAdaptor();
-		setListView();
-
-		loadNetworks();
-	}
-
-	private void setScanAdaptor() {
-		if (scanAdaptor == null) {
-			scanAdaptor = new ScanAdapter(this, 0);
-		}
-	}
-
-	private void setListView() {
-		// basic setup of the ListView and adapter
-		final ListView lv = (ListView) findViewById(R.id.list);
-		lv.setAdapter(scanAdaptor);
-		lv.setEmptyView(findViewById(R.id.empty_element));
-		lv.setClickable(true);
-		lv.setLongClickable(true);
-		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
-
-				TextView textView = (TextView) view.findViewById(R.id.scan_connected);
-
-				if (textView.getText().equals("Connected")) {
-					return;
-				}
-
-				final ScanRating result = (ScanRating) parent.getItemAtPosition(position);
-
-				if (!WifiUtil.connectToNetwork(getApplicationContext(), result.getScanResult().SSID)) {
-					startActivity(new Intent(WifiManager.ACTION_PICK_WIFI_NETWORK));
-				}
-				for (int i = 0; i < lv.getCount(); i++) {
-					View child = lv.getChildAt(i);
-					if (child != null) {
-						TextView clearText = (TextView) findViewById(R.id.scan_connected);
-						clearText.setText("");
-					}
-				}
-
-				textView.setText("Connecting...");
-			}
-
-		});
-
-		lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-			@Override
-			public boolean onItemLongClick(AdapterView<?> parent, final View view, int position, long id) {
-				ScanRating scanRating = (ScanRating) parent.getItemAtPosition(position);
-				Intent intent = new Intent(view.getContext(), ScanDetailActivity.class);
-				Bundle bundle = new Bundle();
-				bundle.putSerializable(scanRating.getRating().SER_KEY, scanRating.getRating());
-				intent.putExtras(bundle);
-				startActivity(intent);
-
-				return true;
-			}
-		});
-	}
-
-	public void onToggleClicked() {
-		boolean enable = !WifiUtil.isWifiEnabled(this);
-		if (enable) {
-			setWifiOnIcon();
-			WifiUtil.enableWifi(this);
-			loadNetworks();
-		} else {
-			setWifiOffIcon();
-			setnoWifiText();
-			scanAdaptor.clear();
-			WifiUtil.disableWifi(this);
-		}
-	}
-
-	private void loadNetworks() {
-
-		if (WifiUtil.isWifiEnabled(this)) {
-			setWifiOnIcon();
-		} else {
-			setnoWifiText();
-			setWifiOffIcon();
-			scanAdaptor.clear();
-			return;
-		}
-
-		final List<ScanResult> results = ReportUtil.getNewScanResults(this, scanAdaptor);
-
-		if (results.size() == 0) {
-			setEmptyText();
-			return;
-		}
-
-		String url = ReportUtil.getScanRatingUrl(results);
-		Builders.Any.B ion = Ion.with(this, url);
-		ion.setTimeout(2000);
-		loading = ion.asJsonObject().setCallback(new FutureCallback<JsonObject>() {
-			@Override
-			public void onCompleted(Exception e, JsonObject jsonRepsonse) {
-				List<ScanRating> ratings = new ArrayList<ScanRating>();
-				if (e != null) {
-					Log.i(Util.LOG_TAG, e.toString());
-
-					for (ScanResult result : results) {
-						ratings.add(new ScanRating(result, result.SSID));
-					}
-				} else {
-					Log.i(Util.LOG_TAG, "Found ratings: " + jsonRepsonse.toString());
-
-					for (ScanResult result : results) {
-						JsonElement rating = jsonRepsonse.get(Util.fmtBSSID(result.BSSID));
-						if (rating != null) {
-							ratings.add(new ScanRating(result, rating.getAsJsonObject()));
-						}
-					}
-				}
-				scanAdaptor.clear();
-				scanAdaptor.addAll(ratings);
-			}
-		});
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		this.menu = menu;
-		if (WifiUtil.isWifiEnabled(this)) {
-			setWifiOnIcon();
-		} else {
-			setWifiOffIcon();
-		}
-
-		return super.onCreateOptionsMenu(menu);
+	protected void onSaveInstanceState(final Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putInt(CURRENT_TAB, currentTab);
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		Intent i;
-		switch (item.getItemId()) {
-
-		case R.id.refreshScan:
-			loadNetworks();
-			return true;
-
-		case R.id.toggleWifi:
-			onToggleClicked();
-			return true;
-
-		case R.id.action_settings:
-			i = new Intent(this, SettingsActivity.class);
-			startActivity(i);
-			return true;
-
-		case R.id.viewHistory:
-			i = new Intent(this, HistoryActivity.class);
-			startActivity(i);
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
+	public void onTabSelected(final Tab tab, final FragmentTransaction ft) {
+		currentTab = tab.getPosition();
+		if (tab == networks) {
+			ft.replace(R.id.content, new ScanFragment());
 		}
-	}
-
-	private void setnoWifiText() {
-		setText(R.string.no_wifi);
-	}
-
-	private void setEmptyText() {
-		setText(R.string.no_networks);
-	}
-
-	private void setText(int resource) {
-		TextView textView = (TextView) findViewById(R.id.empty_element);
-		if (textView != null) {
-			textView.setText(resource);
-		}
-
-	}
-
-	private void setWifiOffIcon() {
-		setWifiIcon(R.drawable.ic_action_network_cell);
-	}
-
-	private void setWifiOnIcon() {
-		setWifiIcon(R.drawable.ic_action_network_wifi);
-	}
-
-	private void setWifiIcon(int resource) {
-		if (menu == null) {
-			return;
-		}
-
-		MenuItem item = menu.findItem(R.id.toggleWifi);
-		item.setIcon(resource);
-
 	}
 
 	@Override
-	public void onResume() {
-		super.onResume();
-		autoUpdate = new Timer();
-		autoUpdate.schedule(new TimerTask() {
-			@Override
-			public void run() {
-				runOnUiThread(new Runnable() {
-					public void run() {
-						loadNetworks();
-					}
-				});
-			}
-		}, 0, TWENTY_SECONDS);
+	public void onTabUnselected(final Tab tab, final FragmentTransaction ft) {
+		// don't care
 	}
 
 	@Override
-	public void onPause() {
-		autoUpdate.cancel();
-		super.onPause();
+	public void onTabReselected(final Tab tab, final FragmentTransaction ft) {
+		// don't care
 	}
 }
